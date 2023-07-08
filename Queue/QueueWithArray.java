e60df921e32ae3c4eab78d6e45b03570c43421df
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-07-08 22:47:56 +0545

    Learning Queue

---
+++ Queue/QueueWithArray.java
@@ -0,0 +1,52 @@
+package Queue;
+
+public class QueueWithArray {
+
+    static class Queue {
+
+        static int arr[];
+        static int size;
+        static int rear = -1;
+
+        Queue(int n) {
+            arr = new int[n];
+            this.size = n;
+        }
+
+        public static boolean isEmpty() {
+            return rear == -1;
+        }
+
+        public static void add(int data) {
+            if (rear == size - 1) {
+                System.out.println("Queue is full");
+                return;
+            }
+            rear++;
+            arr[rear] = data;
+        }
+
+        public static int remove() {
+
+            if (isEmpty()) {
+                return -1;
+
+            }
+            int front = arr[0];
+            for (int i = 0; i < rear; i++) {
+                arr[i] = arr[i + 1];
+            }
+            rear--;
+            return front;
+        }
+
+        // public static peek(){
+
+        // }
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("s");
+    }
+}
---
+++ Queue/queue.md
@@ -0,0 +1,8 @@
+# Queue
+![Alt text](https://media.geeksforgeeks.org/wp-content/cdn-uploads/20221213113312/Queue-Data-Structures.png)
+## The methods of queue are
+### Enqueue : This method adds an element to the back of the queue. 0(1)
+### Dequeue : This operation removes and returns the element at the front of the queue. 0(n)
+### Front (or Peek) : This operation returns the element at the front of the queue without removing it. 0(1)
+### isEmpty : This method checks if the stack is empty. 0(1)
+### isSize : This method returns the number of elements in the stack. 0(1)
